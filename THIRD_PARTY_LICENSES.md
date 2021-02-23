e5ebf89728da354641a605c7217fa8d92772dd78
pwirth <wirth@hallowelt.biz>
2021-02-23 16:00:22 +0100

    Init commit

---
+++ THIRD_PARTY_LICENSES.md
@@ -0,0 +1,20 @@
+Third Party Licenses
+====================
+This is an overview of all the 3rd party licenses used in BlueSpicePlayer.
+
+Software
+------
+
+| Software | Author | Version | License |
+|:--------|:-------|:--------|:-------|
+| [VideoJS](https://github.com/videojs/video.js) | Brightcove | 5.11.0 | [Apache 2.0](https://github.com/videojs/video.js/blob/master/LICENSE) |
+
+License texts
+=============
+All licenses can be found in full text in the folder
+
+```BlueSpiceFoundation/doc/legal```
+
+or online at
+
+https://github.com/wikimedia/mediawiki-extensions-BlueSpiceFoundation/tree/master/doc/legal
---
+++ src/Tag/ShowTime.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace BlueSpice\Player\Tag;
+
+use BlueSpice\Tag\IHandler;
+use BlueSpice\Tag\Tag;
+use BlueSpice\ParamProcessor\ParamDefinition;
+use MediaWiki\MediaWikiServices;
+use RepoGroup;
+
+class ShowTime extends Tag {
+
+	/**
+	 * @var \Config
+	 */
+	private $config = null;
+
+	/**
+	 * @param null $config
+	 */
+	public function __construct( $config = null ) {
+		$this->config = $config;
+		if ( $this->config === null ) {
+			$this->config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		}
+	}
+
+	/**
+		* @return string[]
+		*/
+	public function getTagNames() {
+		return [ 'showtime', 'bs:showtime' ];
+	}
+
+	/**
+	 * @param mixed $processedInput
+	 * @param array $processedArgs
+	 * @param \Parser $parser
+	 * @param \PPFrame $frame
+	 *
+	 * @return IHandler
+	 */
+	public function getHandler( $processedInput, array $processedArgs, \Parser $parser,
+																		\PPFrame $frame ) {
+		$repoGroup = RepoGroup::singleton();
+		return new ShowTimeHandler( $processedInput, $processedArgs, $parser, $frame, $repoGroup );
+	}
+
+	/**
+	 * @return array|\BlueSpice\ParamProcessor\IParamDefinition[]
+	 * @throws \ConfigException
+	 */
+	public function getArgsDefinitions() {
+		$defaultWidth = $this->config->get( 'ShowtimePrefWidth' );
+		$defaultHeight = $this->config->get( 'ShowtimePrefHeight' );
+		return [
+			new ParamDefinition( 'integer', 'width', $defaultWidth ),
+			new ParamDefinition( 'integer', 'height', $defaultHeight ),
+			new ParamDefinition( 'boolean', 'autostart',  false ),
+			new ParamDefinition( 'boolean', 'repeat', false )
+		];
+	}
+
+	/**
+	 * Load resources
+	 * @return array|string[]
+	 */
+	public function getResourceLoaderModules() {
+		return [
+			'ext.bluespice.showtime'
+		];
+	}
+
+}
