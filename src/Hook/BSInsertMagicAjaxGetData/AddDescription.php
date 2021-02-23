4b34411b6af06df147c043b95d589729cd07350e
pwirth <wirth@hallowelt.biz>
2021-02-23 13:39:12 +0100

    Initial commit

---
+++ src/Hook/BSInsertMagicAjaxGetData/AddDescription.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace BlueSpice\Player\Hook\BSInsertMagicAjaxGetData;
+
+use BlueSpice\InsertMagic\Hook\BSInsertMagicAjaxGetData;
+
+class AddDescription extends BSInsertMagicAjaxGetData {
+
+	protected function doProcess() {
+		if ( $this->type != 'tags' ) {
+			return true;
+		}
+
+		$this->response->result[] = [
+			'id' => 'showtime',
+			'type' => 'tag',
+			'name' => 'showtime',
+			'desc' => $this->getContext()->msg( 'bs-showtime-tag-showtime-desc' )->plain(),
+			'code' => '<bs:showtime width="320" height="240" >Placeholder.mp4</bs:showtime>',
+			'helplink' => 'https://en.wiki.bluespice.com/wiki/Reference:Player'
+		];
+
+		return true;
+	}
+}
