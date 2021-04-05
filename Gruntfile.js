e71f816f63b81a9272aef2a66fa40b8c7b0bef75
Umherirrender <umherirrender_de.wp@web.de>
2021-04-05 22:04:11 +0200

    build: Replace jsonlint with eslint
    The gitignore for node_modules is needed for libup
    
    Bug: T220036
    Change-Id: I88c8daa953312aaead493a6fdcdbc7ba57218e3f

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,22 @@
+/* eslint-env node, es6 */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	var conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
-	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.initConfig( {
 		banana: conf.MessagesDirs,
-		jsonlint: {
+		eslint: {
 			all: [
 				'**/*.json',
 				'!node_modules/**',
-				'!vendor/**'
+				'!vendor/**',
+				'!resources/vendor/**'
 			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
