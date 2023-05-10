5c5f1e6151d8e2bcb00edfda1f884d712176909e
pavloyashchenko <pyashchenko@hallowelt.com>
2023-05-10 15:15:29 +0200

    Fix autoplay setting for players
    ERM32004
    
    [4.2.x, 4.3.x]
    
    Change-Id: I2b4f00c34ba6dc1e30f4975c2f03d4780bb79297

--- resources/bluespice.bluespiceplayer.js
+++ resources/bluespice.bluespiceplayer.js
@@ -1,6 +1,7 @@
 (function( mw, $, d, undefined ) {
     $( function() {
       $( '.bs-video' ).each( function() {
+          this.autoplay = mw.user.options.get( 'bs-showtime-pref-autostart' );
           new Plyr( this );
       } );
     } );
