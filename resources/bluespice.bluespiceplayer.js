56e218b18b2465d98525f824f8a97b898a1d17c3
d.savuljesku <d.savuljesku@gmail.com>
2023-06-06 14:53:27 +0200

    Make sure to pass bool to Plyr config
    [4.2.x]
    
    ERM32004
    
    Change-Id: I8c180f5085d84152c7bd65ca9c5d55b60b53df36
    (cherry picked from commit 2b345d358dd7767b75c09add4dd78bba8109bab8)
    (cherry picked from commit 12afd22c88940fb8929a4da1ef0ee99bc6f793c6)
    (cherry picked from commit 2d6091d5ab516ade2d4b5ac77df42bafe74861f7)

--- resources/bluespice.bluespiceplayer.js
+++ resources/bluespice.bluespiceplayer.js
@@ -1,7 +1,10 @@
 (function( mw, $, d, undefined ) {
-    $( function() {
-      $( '.bs-video' ).each( function() {
-          new Plyr( this );
-      } );
-    } );
+	$( function() {
+		$( '.bs-video' ).each( function() {
+			this.autoplay = mw.user.options.get( 'bs-showtime-pref-autostart' ) === '1';
+			// Nobody wants to get blasted with sound on an autoplay video
+			this.muted = this.autoplay;
+			new Plyr( this );
+		} );
+	} );
 })( mediaWiki, jQuery, document );
